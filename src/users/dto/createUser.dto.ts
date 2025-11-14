8497923ac6a5350c1720fdbd87c177782f59ad1e
Jediael Borges <jediaelborges23@gmail.com>
2025-11-14 16:41:22 -0300

    feat: use global pipes

--- src/users/dto/createUser.dto.ts
+++ src/users/dto/createUser.dto.ts
@@ -1,1 +1,16 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    fullName: string;
+    @IsString()
+    phone: string;
+    @IsEmail()
+    email: string;
+    @IsString()
+    password: string;
+    @IsString()
+    objectives: string;
+    @IsString()
+    prognosys: string;
+}
