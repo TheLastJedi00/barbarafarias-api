61605d56e3b435f5a1758b09126ce946d8140b7c
Jediael Borges <jediaelborges23@gmail.com>
2025-11-23 14:43:43 -0300

    feat: function generateContent is ready

--- src/supply/domain/genai.port.ts
+++ src/supply/domain/genai.port.ts
@@ -1,3 +1,6 @@
+import { StudentInfo } from "./types/student.info";
+import { Module } from "./types/student.supply";
+
 export interface GenerativeAIService {
-    generateContent(prompt: string): Promise<string>;
+    generateContent(prompt: string, studentInfo: StudentInfo): Promise<Module[]>;
 }
--- src/supply/infrastructure/gemini.ts
+++ src/supply/infrastructure/gemini.ts
@@ -1,30 +1,52 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
-import { GenerativeAIService } from "../domain/genai.port";
-import { ConfigService } from "@nestjs/config";
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import { GenerativeAIService } from '../domain/genai.port';
+import { ConfigService } from '@nestjs/config';
+import { StudentInfo } from '../domain/types/student.info';
+import { Module } from '../domain/types/student.supply';
 
 export class GeminiProvider implements GenerativeAIService {
-    private genAI: GoogleGenerativeAI;
-    private model: any;
+  private genAI: GoogleGenerativeAI;
+  private model: any;
 
-    constructor(private readonly configService: ConfigService) {
-        const apiKey = this.configService.get<string>('GEMINI_API_KEY');
-        if (!apiKey) {
-            throw new Error("GEMINI_API_KEY is not defined in environment variables");
-        }
-        this.genAI = new GoogleGenerativeAI(apiKey);
-        const modelName = this.configService.get<string>('GEMINI_MODEL') || 'gemini-3-pro-preview';
-
-        this.model = this.genAI.getGenerativeModel({
-            model: modelName,
-            generationConfig: {
-                responseMimeType: 'application/json'
-            }
-        });
+  constructor(private readonly configService: ConfigService) {
+    const apiKey = this.configService.get<string>('GEMINI_API_KEY');
+    if (!apiKey) {
+      throw new Error('GEMINI_API_KEY is not defined in environment variables');
     }
+    this.genAI = new GoogleGenerativeAI(apiKey);
+    const modelName =
+      this.configService.get<string>('GEMINI_MODEL') || 'gemini-3-pro-preview';
 
-    generateContent(prompt: string): Promise<string> {
-        throw new Error("Method not implemented.");
+    this.model = this.genAI.getGenerativeModel({
+      model: modelName,
+      generationConfig: {
+        responseMimeType: 'application/json',
+      },
+    });
+  }
+
+  async generateContent(prompt: string, studentInfo: StudentInfo): Promise<Module[]> {
+    let cleanText = '';
+    try {
+      const fullPrompt = `${prompt}\n
+        Dados do aluno:\n
+        Nome: ${studentInfo.firstName}\n
+        Objetivos pessoais do aluno: ${studentInfo.objectives}\n
+        Prognóstico da Teacher: ${studentInfo.prognosis}`;
+      const result = await this.model.generateContent(fullPrompt);
+      const response = await result.response;
+      const text: string = response.text();
+      cleanText = text
+        .replace(/```json/g, '')
+        .replace(/```/g, '')
+        .trim();
+      const modules: Module[] = JSON.parse(cleanText);
+      return modules;
+    } catch (error) {
+      console.error('Falha ao processar resposta do Gemini.');
+      console.error('Erro:', error);
+      console.error('Texto recebido (Raw):', cleanText);
+      throw new Error('Failed to generate content with Gemini');
     }
-    
-
+  }
 }
