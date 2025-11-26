315d79634b45519663aa0011f2d2eccd809ed542
Jediael Borges <jediaelborges23@gmail.com>
2025-11-26 18:25:49 -0300

    fix: incorrect inject in teacherService contructor

--- src/teacher/application/teacher.service.ts
+++ src/teacher/application/teacher.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 import { Teacher } from "../domain/teacher.model";
 import type { TeacherRepository } from "../domain/teacherRepository.port";
 
 @Injectable()
 export class TeacherService {
     constructor(
+        @Inject('TeacherRepository')
         private readonly teacherRepository: TeacherRepository
     ) {}
 
