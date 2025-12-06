ba776231a3ac5052d80017faa1204ff342c40456
Jediael Borges <jediaelborges23@gmail.com>
2025-12-06 14:37:39 -0300

    fix: export user service

--- src/auth/application/auth.module.ts
+++ src/auth/application/auth.module.ts
@@ -1,13 +1,18 @@
-import { Module } from "@nestjs/common";
-import { FirebaseAuthGuard } from "../infrastructure/firebase.guard";
-import { RolesGuard } from "../infrastructure/roles.guard";
-import { UserService } from "src/users/application/user.service";
-import { TeacherService } from "src/teacher/application/teacher.service";
+import { Module } from '@nestjs/common';
+import { FirebaseAuthGuard } from '../infrastructure/firebase.guard';
+import { RolesGuard } from '../infrastructure/roles.guard';
+import { UserService } from 'src/users/application/user.service';
+import { TeacherService } from 'src/teacher/application/teacher.service';
 
 @Module({
-    providers: [ FirebaseAuthGuard, RolesGuard, UserService, TeacherService ],
-    exports: [ FirebaseAuthGuard, RolesGuard ],
-    controllers: [],
-    imports: [],
+  providers: [
+    FirebaseAuthGuard,
+    RolesGuard,
+    UserService,
+    TeacherService,
+  ],
+  exports: [FirebaseAuthGuard, RolesGuard],
+  controllers: [],
+  imports: [],
 })
 export class AuthModule {}
--- src/users/application/user.module.ts
+++ src/users/application/user.module.ts
@@ -1,18 +1,20 @@
-import { Module } from "@nestjs/common";
-import { UserController } from "./user.controller";
-import { UserFirestoreRepository } from "../infrastructure/user.repository";
-import { UserService } from "./user.service";
-import { IsEmailUnique } from "../infrastructure/validators/IsEmailUnique.constraint";
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { UserFirestoreRepository } from '../infrastructure/user.repository';
+import { UserService } from './user.service';
+import { IsEmailUnique } from '../infrastructure/validators/IsEmailUnique.constraint';
 
 @Module({
   imports: [],
   controllers: [UserController],
   providers: [
-    UserService, {
-    provide: 'UserRepository', 
-    useClass: UserFirestoreRepository},
-    IsEmailUnique
+    UserService,
+    {
+      provide: 'UserRepository',
+      useClass: UserFirestoreRepository,
+    },
+    IsEmailUnique,
   ],
-  exports: [ 'UserRepository' ]
+  exports: ['UserRepository', UserService],
 })
 export class UserModule {}
