788ba112dcdefb850c755269e99b0af246f7e872
Jediael Borges <jediaelborges23@gmail.com>
2025-12-10 13:45:51 -0300

    feat: add uid paramether for userUpdate DTO

--- src/users/application/dto/UpdateUser.dto.ts
+++ src/users/application/dto/UpdateUser.dto.ts
@@ -1,10 +1,39 @@
+import { IsBoolean, IsEmail, IsOptional, IsString } from "class-validator";
+
 export class UpdateUserDto {
+    @IsString()
+    @IsOptional()
+    id: string;
+
+    @IsString()
+    @IsOptional()
     fullName: string;
+
+    @IsString()
+    @IsOptional()
     phone: string;
+
+    @IsEmail()
+    @IsOptional()
     email: string;
+
+    @IsBoolean()
+    @IsOptional()
     isPaying: boolean;
+
+    @IsBoolean()
+    @IsOptional()
     isTeacher: boolean;
+
+    @IsString()
+    @IsOptional()
     level: string;
+
+    @IsString()
+    @IsOptional()
     objectives: string;
+
+    @IsString()
+    @IsOptional()
     prognosis: string;
 }
